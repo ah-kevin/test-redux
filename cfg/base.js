21807d07bb08d36091b4bbadf044ef0aa38aff29
dg_lennon <kbjwdh@gmail.com>
2016-06-05 11:20:21 +0800

    修改webpack colors

--- cfg/base.js
+++ cfg/base.js
@@ -25,7 +25,10 @@ module.exports = {
     hot: true,
     port: defaultSettings.port,
     publicPath: defaultSettings.publicPath,
-    noInfo: false
+    noInfo: false,
+    stats:{
+      colors:true
+    }
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
