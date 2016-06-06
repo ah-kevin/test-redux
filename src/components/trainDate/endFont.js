956c2b8a233cf3aacc4ed56285956a871c9ce2a1
dg_lennon <kbjwdh@gmail.com>
2016-06-06 21:16:55 +0800

    样式重构完成

--- src/components/trainDate/endFont.js
+++ src/components/trainDate/endFont.js
@@ -7,12 +7,16 @@ import {immutableRenderDecorator} from 'react-immutable-render-mixin';
 class endFont extends Component {
   render () {
     return (
-      <div className="out" style={{
-        backgroundColor:this.props.color,
-        borderColor:this.props.color
-      }}>
-        <div className="in">{this.props.value}</div>
+      <div>
+          <div className="out" >
+            <div className="in" style={{
+              backgroundColor:this.props.color,
+              borderColor:this.props.color
+            }}>{this.props.value}</div>
+          </div>
+        <div className="line"></div>
       </div>
+
     );
   }
 }
--- src/components/trainDate/trainItem.js
+++ src/components/trainDate/trainItem.js
@@ -5,7 +5,7 @@ import React, {
   Component,
   PropTypes
 } from 'react';
-import {Timeline,Row,Col,Icon} from 'antd';
+import {Row,Col,Icon} from 'antd';
 import EndFont from './endFont';
 
 import {immutableRenderDecorator} from 'react-immutable-render-mixin';
@@ -15,23 +15,20 @@ class trainItem extends Component {
     const {items} =this.props;
     let itemslength=items.size;
     let TimelineNodes=items.map((item,index)=>
-      <Timeline.Item key={index} color="green"
-                     dot={index==0?<EndFont value="始" color="#f9aa05"/>:<EndFont value=""/>&&
-                     index==itemslength-1?<EndFont value="终" color="#39b3e6"/>:<EndFont value=""/>}
-      >
-        <Row type="flex" justify="center" align="midlle">
-          <Col span={7}>{item.get(0)}</Col>
+        <Row type="flex" justify="center" align="middle" key={index} >
+          <Col span={2}>
+           {index==0?<EndFont value="始" color="#f9aa05"/>:<EndFont value="" color="#97e0a1"/>&&
+            index==itemslength-1?<EndFont value="终" color="#39b3e6"/>:<EndFont value=""  color="#97e0a1"/>}
+          </Col>
+          <Col span={5}>{item.get(0)}</Col>
            <Col span={5}>{item.get(1)}</Col>
-           <Col span={7}>{item.get(2)}</Col>
+           <Col span={5}>{item.get(2)}</Col>
            <Col span={5}>{item.get(3)}</Col>
         </Row>
-      </Timeline.Item>
     )
     return (
-      <div>
-        <Timeline>
+      <div className="item">
           {TimelineNodes}
-        </Timeline>
       </div>
     );
   }
